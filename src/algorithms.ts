7aac33d866e4e77cd711939ba86a4ed473cf4872
Frederic Vogels <frederic.vogels@gmail.com>
2018-07-24 11:23:24 +0200

    range, filter, all, any

--- src/algorithms.ts
+++ src/algorithms.ts
@@ -12,3 +12,63 @@ export function count<T>(xs : Iterable<T>, predicate : (t : T) => boolean) : num
 
     return result;
 }
+
+export function range(from : number, to : number) : number[]
+{
+    if ( from >= to )
+    {
+        return new Array<number>(0);
+    }
+    else
+    {
+        const result = new Array(from - to);
+
+        for ( let i = 0; i !== result.length; ++i )
+        {
+            result[i] = from + i;
+        }
+        
+        return result;
+    }
+}
+
+export function filter<T>(xs : Iterable<T>, predicate : (t : T) => boolean) : T[]
+{
+    const result = [];
+
+    for ( let x of xs )
+    {
+        if ( predicate(x) )
+        {
+            result.push(x);
+        }
+    }
+
+    return result;
+}
+
+export function all<T>(xs : Iterable<T>, predicate : (t : T) => boolean) : boolean
+{
+    for ( let x of xs )
+    {
+        if ( !predicate(x) )
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+export function any<T>(xs : Iterable<T>, predicate : (t : T) => boolean) : boolean
+{
+    for ( let x of xs )
+    {
+        if ( predicate(x) )
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
