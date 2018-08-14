d2456649b4c64bf5554f99f6777e67a9c2ef4d36
Frederic Vogels <frederic.vogels@gmail.com>
2018-08-14 14:59:44 +0200

    dist

--- dist/algorithms.d.ts
+++ dist/algorithms.d.ts
@@ -1,6 +1,8 @@
 import { Maybe } from 'maybe';
 export declare function count<T>(xs: Iterable<T>, predicate: (t: T) => boolean): number;
 export declare function range(from: number, to: number): number[];
+export declare function generateRange(from: number, to: number): Iterable<number>;
+export declare function allEqual<T>(xs: T[], equality?: (x: T, y: T) => boolean): boolean;
 export declare function filter<T>(xs: Iterable<T>, predicate: (t: T) => boolean): T[];
 export declare function all<T>(xs: Iterable<T>, predicate: (t: T) => boolean): boolean;
 export declare function any<T>(xs: Iterable<T>, predicate: (t: T) => boolean): boolean;
@@ -10,3 +12,4 @@ export declare function maximumBy<T>(xs: Iterable<T>, f: (t: T) => number): Mayb
 export declare function createArray<T>(length: number, initializer: (i: number) => T): T[];
 export declare function repeat<T>(n: number, x: T): T[];
 export declare function indices<T>(xs: T[]): number[];
+export declare function isPermutation<T>(xs: T[], ys: T[], equality?: (x: T, y: T) => boolean): boolean;
